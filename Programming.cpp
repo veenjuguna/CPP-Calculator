891711ca3fa33c1c377f989f2e221e27b08afc74
Ivy Njuguna <ivynjuguna2006@gmail.com>
2025-05-27 21:55:26 +0300

    Add files via upload

---
+++ Programming.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	printf("Hello team?\n");
+	printf("welcome to programming with c\n");
+	printf("we will be programming in c\n");
+	printf("i hope we have fun\n");
+	printf("all the best\n");
+	return 0;
+}
+	
+	
